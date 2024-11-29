47b2aff7738a353a957e2db62f601f46d7bb1c4a
iffat180 <106329988+iffat180@users.noreply.github.com>
2024-11-28 20:51:40 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/iffat180.github.io/', // Replace with your repository name
   plugins: [react()],
-})
+});
