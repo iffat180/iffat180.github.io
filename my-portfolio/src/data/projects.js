4cab53903c1371db0c7f35a066b14b2a8d0bab89
Iffat Mohamed Ayub <iffat180@gmail.com>
2025-09-17 19:29:55 -0400

    new project added

--- my-portfolio/src/data/projects.js
+++ my-portfolio/src/data/projects.js
@@ -8,5 +8,12 @@ export const projects = [
     stack: ["React", "Node", "MongoDB"],
     link: "https://car-rental-nu-rouge.vercel.app",
     img: car
+  },
+  {
+    name: "Cocktail Menu App",
+    desc: "Description for project 2.",
+    stack: ["React", "GSAP", "Tailwind CSS"],
+    link: "https://cocktail-app-frontend-5pkz3sfcs-iffat180s-projects.vercel.app",
+    // img: "path/to/image2.jpg"
   }
 ];
